ef3343d24cd12d980ab0328b069397b3e0b275b2
Nico Wimmer <nico.wimmer@maibornwolff.de>
2023-10-25 16:36:43 +0200

    move common logic to own file

---
+++ common.js
@@ -0,0 +1,12 @@
+
+/**
+ * Toggles the visibility of the hamburger menu.
+ */
+function toggleMenu() {
+    const menu = document.getElementById('menuItems');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
--- diceroller.js
+++ diceroller.js
@@ -80,15 +80,3 @@ function updateLog(message) {
         logList.removeChild(logList.lastChild);
     }
 }
-
-/**
- * Toggles the visibility of the hamburger menu.
- */
-function toggleMenu() {
-    const menu = document.getElementById('menuItems');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-}
